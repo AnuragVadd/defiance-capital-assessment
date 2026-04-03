ab9aac9b9d69aacc76f46305f2613c92d77496e9
AnuragVadd <anurag.vadd@gmail.com>
2026-04-03 19:58:43 +0530

    all tasks completed

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('chai-str');
-require('dotenv').config();
-const { connectDB } = require('./config/db.js');
-const userRouter = require('./routes/userRoute.js');
-const foodRouter = require('./routes/foodRoute.js');
-const cartRouter = require('./routes/cartRoute.js');
-const orderRouter = require('./routes/orderRoute.js');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("chai-str");
+require("dotenv").config();
+const { connectDB } = require("./config/db.js");
+const userRouter = require("./routes/userRoute.js");
+const foodRouter = require("./routes/foodRoute.js");
+const cartRouter = require("./routes/cartRoute.js");
+const orderRouter = require("./routes/orderRoute.js");
 
 // app config
 const app = express();
@@ -21,15 +21,15 @@ connectDB();
 
 // api endpoints
 app.use("/api/food", foodRouter);
-app.use("/images", express.static('uploads'));
-app.use('/api/user', userRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-    res.send("API working");
+  res.send("API working");
 });
 
 app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
+  console.log(`Server started on http://localhost:${port}`);
 });
--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,35 +1,55 @@
-import React, { useContext} from 'react'
-import './FoodItem.css'
-import { assets } from '../../assets/assets'
-import { StoreContext } from '../context/StoreContext';
+import React, { useContext } from "react";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../context/StoreContext";
 
-const FoodItem = ({id,name,price,description,image}) => {
-
-    const {cartItems,addToCart,removeFromCart,url} = useContext(StoreContext);
+const FoodItem = ({ id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeFromCart, url } =
+    useContext(StoreContext);
 
   return (
-    <div className='food-item'>
-        <div className="food-item-img-container">
-            <img className='food-item-image' src={url+'/images/'+image} alt="" />
-            {
-                !cartItems[id] ? <img className='add' onClick={()=> addToCart(id)} src={assets.add_icon_white}/>: 
-                <div className="food-item-counter">
-                    <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
-                    <p>{cartItems[id]}</p>
-                    <img onClick={()=>addToCart(id)}  src={assets.add_icon_green} alt="" />
-                </div>
-            }
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img
+          className="food-item-image"
+          // src={url+'/images/'+image}
+          src={
+            "https://images.openai.com/static-rsc-4/SntiNdj3hpG4Wv9IsYwLOiSP35R7A9e1U1q2nxkhVjD-z_49Ls4jf6D7hSok4GwBw_RWxMUN2TFxbYY7kBsSas89E_f2HZ6UCsK5WG6S-ANfN_RaNmvPucj0S0AcHCRnpznhPYmumRZLcu44F9GjilWy8I0eI5xltBiVdnxtZOXhFIHZzmT0hMgek6wPTUUs?purpose=inline"
+          }
+          alt=""
+        />
+        {!cartItems[id] ? (
+          <img
+            className="add"
+            onClick={() => addToCart(id)}
+            src={assets.add_icon_white}
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{cartItems[id]}</p>
+            <img
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
         </div>
-        <div className="food-item-info">
-            <div className="food-item-name-rating">
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt="" />
-            </div>
-            <p className="food-item-desc">{description}</p>
-            <p className="food-item-price">${price}</p>
-        </div>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodItem
+export default FoodItem;
