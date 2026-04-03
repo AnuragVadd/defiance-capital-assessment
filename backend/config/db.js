7835e3ac51309f7b3a0d4662ed80e8c8118bdfe9
AnuragVadd <anurag.vadd@gmail.com>
2026-04-03 19:09:36 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      // "mongodb+srv://dulanjalisenarathna93:E2JUb0zfaT2FVp8D@cluster0.exkxkun.mongodb.net/reactjs-food-delivery-app",
+      "mongodb://localhost:27017/defiance-capital",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    );
+    console.log("DB connected");
+  } catch (err) {
+    console.error("DB connection failed:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = { connectDB };
